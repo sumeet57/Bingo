6da639324a18565f7e67706c3a2edcabf7d590ec
sumeet57 <sumeetumbalkar57@gmail.com>
2024-08-22 21:24:26 +0530

    initial setup commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="text-black font-extrabold text-7xl">hdbc</div>
+    </>
+  );
+};
+
+export default App;
